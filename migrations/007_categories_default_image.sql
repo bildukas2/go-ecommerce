bc667c160cb3971d352dc6f981437fd6e04ac39f
Bildukas <andrius.ran@gmail.com>
2026-02-18 00:18:40 +0200

    Initialize task: product default image

--- migrations/007_categories_default_image.sql
+++ migrations/007_categories_default_image.sql
@@ -3,15 +3,14 @@ ALTER TABLE categories
 ADD COLUMN IF NOT EXISTS default_image_url text NULL;
 
 UPDATE categories c
-SET default_image_url = pick.url
-FROM LATERAL (
+SET default_image_url = (
   SELECT i.url
   FROM product_categories pc
   JOIN images i ON i.product_id = pc.product_id
   WHERE pc.category_id = c.id
   ORDER BY i.sort ASC, i.id ASC
   LIMIT 1
-) pick
+)
 WHERE c.default_image_url IS NULL;
 
 -- +goose Down
